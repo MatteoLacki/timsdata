6f883eb209d7ff06cfb3b16677e16644b7458003
matteo <matteo.lacki@gmail.com>
2019-11-14 15:22:30 +0100

    Moved some code from MIDIA that does not require more than numpy installed.

--- timsdata/timsdata.py
+++ timsdata/timsdata.py
@@ -5,8 +5,11 @@ import numpy as np
 import sqlite3
 import os, sys
 from ctypes import *
+from functools import lru_cache
 from pkg_resources import resource_filename as get_so_dll
+from pathlib import Path
 from platform import architecture, system as get_system
+
 
 system = get_system()
 arch32or64, plat = architecture()
@@ -90,29 +93,35 @@ def decodeArrayOfStrings (blob):
         
 class TimsData:
     def __init__ (self, analysis_directory, use_recalibrated_state=False):
+        """Initialize TimsData.
 
+        Args:
+            analysis_directory (str, unicode string): path to the folder containing 'analysis.tdf'
+            use_recalibrated_state: ???
+        """
         if sys.version_info.major == 2:
             if not isinstance(analysis_directory, unicode):
                 raise ValueError("analysis_directory must be a Unicode string.")
         if sys.version_info.major == 3:
-            if not isinstance(analysis_directory, str):
-                raise ValueError("analysis_directory must be a string.")
+            analysis_directory = Path(analysis_directory)
 
         self.dll = dll
 
         self.handle = self.dll.tims_open(
-            analysis_directory.encode('utf-8'),
+            str(analysis_directory).encode('utf-8'),
             1 if use_recalibrated_state else 0 )
         if self.handle == 0:
             throwLastTimsDataError(self.dll)
 
-        self.conn = sqlite3.connect(os.path.join(analysis_directory, "analysis.tdf"))
+        self.conn = sqlite3.connect(str(analysis_directory/"analysis.tdf"))
 
         self.initial_frame_buffer_size = 128 # may grow in readScans()
 
     def __del__ (self):
+        """Destructor."""
         if hasattr(self, 'handle'):
             self.dll.tims_close(self.handle)
+        self.conn.close()
             
     def __callConversionFunc (self, frame_id, input_data, func):
 
@@ -153,42 +162,69 @@ class TimsData:
     def voltageToScanNum (self, frame_id, mzs):
         return self.__callConversionFunc(frame_id, mzs, self.dll.tims_voltage_to_scannum)
 
-        
-    # Output: list of tuples (indices, intensities)
-    def readScans (self, frame_id, scan_begin, scan_end):
+    def get_peakCnts_massIdxs_intensities_array(self,
+                                                frame,
+                                                scan_begin,
+                                                scan_end,
+                                                cut = False):
+        """Get a numpy array with frame data.
 
+        The first 'scan_end-scan_begin' positions contain counts of peaks per scan.
+        Sum it to get the total number of peaks in a frame.
+        After that, come the mass indices (recorded time) of the first scan, then intensities.
+        Then the next scan and so on, until the last.
+        For instance, for 3 scans it could look like:
+        3 2 1 -1002434 -1002442 -4343332 9 10 23 13200234 2342421 2323424 9 9 10 33535423 10
+
+        Args:
+            frame (int): Frame number.
+            scan_begin (int): Lower scan.
+            scan_end (int): Upper scan.
+            cut (bool): cut the outcome to contain exactly the required data and nothing else. This takes additional 25% of time. 
+        Returns:
+            numpy.array: Frame (TIMS push) measurements.
+        """
         # buffer-growing loop
         # shouldn't this be in C???
         while True:
-            cnt = int(self.initial_frame_buffer_size) # necessary cast to run with python 3.5
+            cnt = int(self.initial_frame_buffer_size) # for python 3.5
             buf = np.empty(shape=cnt, dtype=np.uint32)
             len = 4 * cnt
-
             required_len = self.dll.tims_read_scans_v2(self.handle,
-                                                       frame_id,
-                                                       scan_begin,
-                                                       scan_end,
-                                                    buf.ctypes.data_as(POINTER(c_uint32)),
-                                                    len)
-            if debug:
-                print(buf.ctypes.data_as(POINTER(c_uint32)))
-
+                frame, scan_begin, scan_end,
+                buf.ctypes.data_as(POINTER(c_uint32)), len)
             if required_len == 0:
                 throwLastTimsDataError(self.dll)
-
             if required_len > len:
-                if required_len > 16777216:
-                    # arbitrary limit for now...
+                if required_len > 16777216: # arbitrary limit for now...
                     raise RuntimeError("Maximum expected frame size exceeded.")
                 self.initial_frame_buffer_size = required_len / 4 + 1 # grow buffer
             else:
                 break
+        if cut:
+            d = scan_end-scan_begin
+            return buf[0:int(d+buf[:d].sum())]
+        else:
+            return buf
+        
+    
+    def readScans (self, frame_id, scan_begin, scan_end):
+        """Read a selection of scans from a given frame.
 
+        Args:
+            frame_id (int): Frame number.
+            scan_begin (int): Lower scan.
+            scan_end (int): Upper scan.
+        Returns:
+            A list of tuples. Each tuple consists of two numpy arrays: m/z indices and intensities.
+        """
+        buf = self.get_peakCnts_massIdxs_intensities_array(frame_id,
+                                                           scan_begin,
+                                                           scan_end)
         if debug:   
             print(buf)
             print(type(buf))
             print(buf.shape)
-
         result = []
         d = scan_end - scan_begin
         for i in range(scan_begin, scan_end):
@@ -198,13 +234,119 @@ class TimsData:
             intensities = buf[d : d+npeaks]
             d += npeaks
             result.append((indices,intensities))
-
         return result
 
 
+    def iterScans(self, frame, scan_begin, scan_end):
+        """An generator of scan spectra, omitting empty scans.
+
+        Args:
+            frame (int, iterable, slice): Frames to output.
+            scan_begin (int): Lower scan.
+            scan_end (int): Upper scan.
+        Yields:
+            The frame id, the mass indices, and the intensities.
+        """
+        buf = self.get_peakCnts_massIdxs_intensities_array(frame,
+                                                           scan_begin,
+                                                           scan_end,
+                                                           False)
+        d = scan_end - scan_begin
+        for i in range(scan_begin, scan_end):
+            npeaks = buf[i-scan_begin]
+            if npeaks > 0:
+                indices = buf[d : d+npeaks]
+                d += npeaks
+                intensities = buf[d:d+npeaks]
+                d += npeaks
+                yield (i, indices, intensities)
+
+    def frame_scan_mzIdx_I_numpy(self, frame, scan_begin, scan_end):
+        """Get a 2D array of data for a given frame and scan region.
+
+        The output array contains four columns: first repeats the frame number,
+        second contains scan numbers, third contains mass indices, and the last contains intensities.
+        
+        Args:
+            frame (int, iterable, slice): Frames to output.
+            scan_begin (int): Lower scan.
+            scan_end (int): Upper scan.
+
+        Returns:
+            numpy.array: four-columns array with data.
+        """
+        x = self.get_peakCnts_massIdxs_intensities_array(frame,
+                                                         scan_begin,
+                                                         scan_end,
+                                                         False)
+        d = scans_no = scan_end-scan_begin
+        peak_cnts = x[:scans_no]
+        peak_cnts = peak_cnts.astype(np.int)
+        tot_len = int(peak_cnts.sum())
+        X = np.empty(shape=(tot_len,4), dtype=np.int) # contains output
+        X[:,0] = frame
+        X[:,1] = np.repeat(np.arange(scan_begin,scan_end), peak_cnts)
+        m = 0
+        for npeaks in peak_cnts[peak_cnts>0]:
+            X[m:m+npeaks,2] = x[d:d+npeaks]
+            d += npeaks
+            X[m:m+npeaks,3] = x[d:d+npeaks]
+            d += npeaks
+            m += npeaks
+        return X
+
+
+    def count_peaks_per_frame_scanRange(self, frame, scan_begin, scan_end):
+        """Count peaks in a given frame and scan range.
+
+        Args:
+            frame (int, iterable, slice): Frames to output.
+            scan_begin (int): Lower scan.
+            scan_end (int): Upper scan.
+
+        Return:
+            int: number of peaks.
+        """
+        x = self.get_peakCnts_massIdxs_intensities_array(frame,
+                                                         scan_begin,
+                                                         scan_end,
+                                                         False)
+        return x[:scan_end-scan_begin].sum()
+
+
+    def get_TIC(self, frame, scan_begin, scan_end, aggregate_scans=False):
+        """Get the total ion count for given scans in a given frame.
+
+        Args:
+            frame (int, iterable, slice): Frames to output.
+            scan_begin (int): Lower scan.
+            scan_end (int): Upper scan.
+            aggregate_scans (bool): Aggregate over scans or leave scans info.
+
+        Returns:
+            np.array or int: scans and intensities, or total intensity in selected scans.
+        """
+        x = self.get_peakCnts_massIdxs_intensities_array(frame,
+                                                         scan_begin,
+                                                         scan_end,
+                                                         False)
+        scans_no = scan_end-scan_begin
+        peaks_cnts = x[:scans_no]
+        if not aggregate_scans:
+            scans = np.nonzero(peaks_cnts)[0]
+        peaks_cnts = peaks_cnts[peaks_cnts > 0].astype(np.int)
+        LR = np.repeat(peaks_cnts,2)
+        LR = np.insert(LR,0,0)
+        LR = LR.cumsum() + scans_no
+        I = np.add.reduceat(x, LR)[1::2]
+        if aggregate_scans:
+            return I.sum()
+        else:
+            return np.column_stack([scans, I])
+
     # read some peak-picked MS/MS spectra for a given list of precursors; returns a dict mapping
     # 'precursor_id' to a pair of arrays (mz_values, area_values).
-    def readPasefMsMs (self, precursor_list):
+    def readPasefMsMs(self, precursor_list):
         precursors_for_dll = np.array(precursor_list, dtype=np.int64)
 
         result = {}
@@ -225,7 +367,7 @@ class TimsData:
 
 		# read peak-picked MS/MS spectra for a given frame; returns a dict mapping
     # 'precursor_id' to a pair of arrays (mz_values, area_values).
-    def readPasefMsMsForFrame (self, frame_id):
+    def readPasefMsMsForFrame(self, frame_id):
         result = {}
 
         @MSMS_SPECTRUM_FUNCTOR
@@ -241,9 +383,7 @@ class TimsData:
         
         return result
 
-    def __del__(self):
-        """Destructor.
-
-        Basically closes the DB connection.
-        """
-        self.conn.close()
+    @lru_cache(maxsize=1)
+    def table_names(self):
+        """Retrieve names of tables in 'analysis.tdf' SQLite3 database."""
+        return [f[0] for f in self.conn.execute(f"SELECT name FROM sqlite_master WHERE TYPE = 'table'")]
