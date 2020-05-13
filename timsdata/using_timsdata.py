bd3d4df8f72c7e1dcdc4adf85272dd55d14eaff5
matteo <matteo.lacki@gmail.com>
2020-05-13 14:47:57 +0200

    updated example

--- timsdata/using_timsdata.py
+++ timsdata/using_timsdata.py
@@ -1,13 +1,10 @@
 """Packaged Bruker's module."""
-%load_ext autoreload
-%autoreload 2
 from timsdata.timsdata import TimsData
+from pprint import pprint
 
 p = '/home/matteo/Projects/bruker/BrukerMIDIA/MIDIA_CE10_precursor/20190912_HeLa_Bruker_TEN_MIDIA_200ng_CE10_100ms_Slot1-9_1_488.d'
 D = TimsData(p)
 
 frame_no, min_scan, max_scan = 100, 0, 918
-D.readScans(frame_no, min_scan, max_scan)
+pprint(D.readScans(frame_no, min_scan, max_scan))
 D.frame_array(frame_no, min_scan, max_scan)
-# it's more documented, too: show timsdata.py
-
